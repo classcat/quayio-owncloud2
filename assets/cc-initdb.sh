a0a5173b0f791bb2d2251fe6d97401c39ff9caeb
ClassCat <support@classcat.com>
2015-06-19 14:57:34 +0900

    cc-initdb.sh, loop for waiting mysql running

--- assets/cc-initdb.sh
+++ assets/cc-initdb.sh
@@ -6,6 +6,7 @@
 ########################################################################
 
 #--- HISTORY -----------------------------------------------------------
+# 19-jun-15 : loop for waiting mysql running.
 # 29-may-15 : Created.
 #-----------------------------------------------------------------------
 
@@ -27,7 +28,14 @@ function init () {
 #############
 
 function config_mysql () {
-  mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql -e "CREATE DATABASE ${MYSQL_OC_DBNAME}"
+
+  RET=1
+  while [[ RET -ne 0 ]]; do
+    sleep 5
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql -e "CREATE DATABASE ${MYSQL_OC_DBNAME}"
+    RET=$?
+  done
+
   mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql -e "CREATE USER '${MYSQL_OC_USERNAME}'@'%' IDENTIFIED BY '${MYSQL_OC_PASSWORD}';"
   mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql -e "GRANT ALL ON ${MYSQL_OC_DBNAME}.* TO '${MYSQL_OC_USERNAME}'@'%'"
 }
@@ -36,7 +44,13 @@ function config_mysql () {
 ### ENTRY POINT ###
 
 init 
-config_mysql
+
+if [ -e /opt/cc-initdb_done ]; then
+  echo "ClassCat Warning >> /opt/cc-initdb_done found, then skip wp configuration."
+else
+  config_mysql
+  touch /opt/cc-initdb_done
+fi
 
 exit 0
 
